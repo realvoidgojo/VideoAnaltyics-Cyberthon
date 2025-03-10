e81e3e836f0b0cef2bc43b6e4ce323ba8a2f0f17
realvoidgojo <dir.harishsivaraman@gmail.com>
2025-03-10 11:18:49 +0530

    Moves `VideoDisplay` component to `pages` directory
    Relocates `VideoDisplay` from `components` to `pages`
    Adjusts import paths accordingly

---
+++ frontend/src/components/VideoUpload.jsx
@@ -0,0 +1,18 @@
+// VideoUpload.jsx
+import React from "react";
+
+const VideoUpload = ({ onVideoUpload, fileInputRef }) => {
+  return (
+    <div>
+      <input
+        type="file"
+        accept="video/*"
+        onChange={onVideoUpload}
+        ref={fileInputRef}
+        className="border border-gray-300 p-2 m-4 rounded-lg bg-black-100"
+      />
+    </div>
+  );
+};
+
+export default VideoUpload;
